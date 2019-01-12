74a3b443bf36357d550f5c90e8789a25ad8db3f8
blakehelton <blake.helton13@gmail.com>
2019-01-11 19:58:09 -0600

    Yeet

--- src/main/kotlin/frc/team1984/robot/Robot.kt
+++ src/main/kotlin/frc/team1984/robot/Robot.kt
@@ -1,9 +1,58 @@
-package frc.team0000.robot
+package frc.team1984.robot
 
 import edu.wpi.first.wpilibj.TimedRobot
+import edu.wpi.first.wpilibj.*
+import edu.wpi.first.wpilibj.command.Scheduler
 
 class Robot : TimedRobot() {
+    companion object {
+        // Sensors
+        val gyro = ADXRS450_Gyro()
+        private val cameraServer = CameraServer.getInstance()
+        val pressureGauge = AnalogInput(1)
+
+        val driverStation: DriverStation = DriverStation.getInstance()
+        val scheduler: Scheduler = Scheduler.getInstance()
+        val prefs: Preferences = Preferences.getInstance()
+    }
+
     override fun robotInit() {
-        println("Hello World from Kotlin!")
+
+    }
+
+    override fun robotPeriodic() {
+        scheduler.run()
+    }
+
+    override fun autonomousInit() {
+
+    }
+
+    override fun autonomousPeriodic() {
+
+    }
+
+    override fun teleopInit() {
+
+    }
+
+    override fun teleopPeriodic() {
+
+    }
+
+    override fun disabledInit() {
+
+    }
+
+    override fun disabledPeriodic() {
+
+    }
+
+    override fun testInit() {
+
+    }
+
+    override fun testPeriodic() {
+
     }
 }
---
+++ src/main/kotlin/frc/team1984/subsystems/Drivetrain.kt
@@ -0,0 +1,5 @@
+package frc.team1984.subsystems
+
+object Drivetrain {
+
+}
